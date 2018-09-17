219e4b99262db50aa027bf29124810571b927837
Jan Petykiewicz <anewusername@gmail.com>
2018-09-16 20:06:57 -0700

    Use readme as long_description

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@
 from setuptools import setup
 import float_raster
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(name='float_raster',
       version=float_raster.version,
       description='High-precision anti-aliasing polygon rasterizer',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Jan Petykiewicz',
       author_email='anewusername@gmail.com',
       url='https://mpxd.net/gogs/jan/float_raster',
