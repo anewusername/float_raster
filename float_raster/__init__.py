89d0611cfc4fa3bff7449d949ccfd4690f778fce
Jan Petykiewicz <jan@mpxd.net>
2024-07-29 00:37:20 -0700

    repeat names for re-export

--- float_raster/__init__.py
+++ float_raster/__init__.py
@@ -5,7 +5,14 @@ Module for rasterizing polygons, with float-precision anti-aliasing on
 See the documentation for float_raster.raster(...) for details.
 """
 
-from .float_raster import *
+from .float_raster import (
+    raster as raster,
+    find_intersections as find_intersections,
+    create_vertices as create_vertices,
+    clip_vertices_to_window as clip_vertices_to_window,
+    get_raster_parts as get_raster_parts,
+    )
+
 
 __author__ = 'Jan Petykiewicz'
 __version__ = '0.7'
